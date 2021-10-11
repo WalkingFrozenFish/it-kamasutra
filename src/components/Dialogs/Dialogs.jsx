d2db2251ef0de31e4c3a046123bccd205773d05b
WalkingFrozenFish <chachin.ol@yandex.kz>
2021-10-11 16:54:13 +0600

    Верстка блока с диалогами

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,9 +2,21 @@ import classes from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
     return (
-        <div>
-            <h2 id={classes.title}>{props.header}</h2>
-            <p className={classes.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident totam architecto nobis cumque vero atque exercitationem asperiores. Corporis, harum sint?</p>
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={`${classes.dialog} ${classes.active}`}>Name 1</div>
+                <div className={classes.dialog}>Name 2</div>
+                <div className={classes.dialog}>Name 3</div>
+                <div className={classes.dialog}>Name 4</div>
+                <div className={classes.dialog}>Name 5</div>
+                <div className={classes.dialog}>Name 6</div>
+                <div className={classes.dialog}>Name 7</div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Lorem ipsum dolor sit amet.</div>
+                <div className={classes.message}>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</div>
+                <div className={classes.message}>Lorem, ipsum dolor.</div>
+            </div>
         </div >
     );
 }
