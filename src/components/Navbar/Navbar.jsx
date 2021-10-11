4c69fc7bab3bea199b654a7e73763d4c0ddce4e7
WalkingFrozenFish <chachin.ol@yandex.kz>
2021-10-11 15:28:58 +0600

    Создание ссылок

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import classes from "./Navbar.module.css";
 
 const Navbar = () => {
@@ -6,19 +7,19 @@ const Navbar = () => {
 
         <nav className={classes.nav}>
             <div className={classes.item}>
-                <a href="/profile">Profile</a>
+                <NavLink to="/profile" activeClassName={classes.active}>Profile</NavLink>
             </div>
             <div className={classes.item}>
-                <a href="/dialogs">Messages</a>
+                <NavLink to="/dialogs" activeClassName={classes.active}>Messages</NavLink>
             </div>
             <div className={classes.item}>
-                <a href="/news">News</a>
+                <NavLink to="/news" activeClassName={classes.active}>News</NavLink>
             </div>
             <div className={classes.item}>
-                <a href="/music">Music</a>
+                <NavLink to="/music" activeClassName={classes.active}>Music</NavLink>
             </div>
             <div className={classes.item}>
-                <a href="/settings">Settings</a>
+                <NavLink to="/settings" activeClassName={classes.active}>Settings</NavLink>
             </div>
         </nav>
     );
