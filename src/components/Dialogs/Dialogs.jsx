fd269f69de334c8f5c93be61cc6d4598299d13b3
WalkingFrozenFish <chachin.ol@yandex.kz>
2021-10-13 15:38:40 +0600

    Рефакторинг

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,28 +3,12 @@ import classes from "./Dialogs.module.css";
 import Message from "./Message/Message";
 
 const Dialogs = (props) => {
-    let dialogs = [
-        { id: 1, name: "Name 1" },
-        { id: 2, name: "Name 2" },
-        { id: 3, name: "Name 3" },
-        { id: 4, name: "Name 4" },
-        { id: 5, name: "Name 5" },
-        { id: 6, name: "Name 6" },
-        { id: 7, name: "Name 7" },
-    ]
 
-    let messages = [
-        { id: 1, message: "Hi" },
-        { id: 2, message: "Lorem ipsum dolor sit amet." },
-        { id: 3, message: "Lorem ipsum dolor sit amet consectetur, adipisicing elit." },
-        { id: 4, message: "Lorem, ipsum dolor." },
-    ]
-
-    let dialogsElements = dialogs.map((item) => {
+    let dialogsElements = props.dialogs.map((item) => {
         return <DialogItem name={item.name} id={item.id} key={item.id} />
     })
 
-    let messagesElements = messages.map((item) => {
+    let messagesElements = props.messages.map((item) => {
         return <Message message={item.message} key={item.id} />
     })
 
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,15 +1,9 @@
 import classes from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
-const MyPosts = () => {
+const MyPosts = (props) => {
 
-    let posts = [
-        { id: 1, likes: 15, message: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi, a.", avatarUrl: "https://images.unsplash.com/photo-1593642532744-d377ab507dc8?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=869&q=80" },
-        { id: 2, likes: 10, message: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas provident soluta voluptates culpa autem est!", avatarUrl: "https://images.unsplash.com/photo-1633824911018-519ad10f2b69?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80" },
-        { id: 3, likes: 21, message: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas provident soluta.", avatarUrl: "https://images.unsplash.com/photo-1634020016130-ab5eca714e69?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=435&q=80" },
-    ]
-
-    let postsElements = posts.map((item) => {
+    let postsElements = props.posts.map((item) => {
         return <Post message={item.message} avatarUrl={item.avatarUrl} likes={item.likes} key={item.id} />
     })
 
